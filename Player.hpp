e65187bbde3a7c80913083162bb8d86d9b14080c
= <mariadelcarmenhernandez@student.hartnell.edu>
2018-05-01 16:21:03 -0700

    Adding header file for player

---
+++ Player.hpp
@@ -0,0 +1,27 @@
+#ifndef Player_hpp
+#define Player_hpp
+
+#include <stdio.h>
+
+#include "Card.hpp"
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Player
+{
+    public:
+        Player();
+        void addCard(vector<Card> cards)// get cards when you don’t have the card you need
+        void addCard(Card c) //?? I was thinking player get one card at a time
+        Card putCard(string color, int number)// send card back
+        void seeMyCard():
+
+    protected:
+    
+        vector<Card> hand;
+    
+    
+}
+
+#endif
