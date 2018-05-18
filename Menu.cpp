f4cd328a58b1f73ee11cbd382bff88424d1f9b00
juhipark <juhipark@student.hartnell.edu>
2018-05-18 12:52:07 -0700

    Fixed everything for final presentation

--- Menu.cpp
+++ Menu.cpp
@@ -7,17 +7,17 @@ Menu::Menu(int w, int h){
     }
     selectedItemIndex  = 1;
     menu[0].setFont(font);
-    menu[0].setColor(sf::Color::Magenta);
+    menu[0].setFillColor(sf::Color::Magenta);
     menu[0].setString("Play");
     menu[0].setPosition(sf::Vector2f(w / 2, h / (MAX_NUMBER + 1) * 1));
 
     menu[1].setFont(font);
-    menu[1].setColor(sf::Color::White);
+    menu[1].setFillColor(sf::Color::White);
     menu[1].setString("Options");
     menu[1].setPosition(sf::Vector2f(w / 2, h / (MAX_NUMBER + 1) * 2));
 
     menu[2].setFont(font);
-    menu[2].setColor(sf::Color::White);
+    menu[2].setFillColor(sf::Color::White);
     menu[2].setString("Exit");
     menu[2].setPosition(sf::Vector2f(w / 2, h / (MAX_NUMBER + 1) * 3));
 
@@ -25,17 +25,17 @@ Menu::Menu(int w, int h){
 
 void Menu::moveUp(){
     if (MAX_NUMBER - 1 <= selectedItemIndex+1 && selectedItemIndex+1 <= MAX_NUMBER){
-        menu[selectedItemIndex].setColor(sf::Color::White);
+        menu[selectedItemIndex].setFillColor(sf::Color::White);
         selectedItemIndex--;
-        menu[selectedItemIndex].setColor(sf::Color::Magenta);
+        menu[selectedItemIndex].setFillColor(sf::Color::Magenta);
     }
 }
 
 void Menu::moveDown(){
     if (MAX_NUMBER - 2 <= selectedItemIndex + 1 && selectedItemIndex + 1 <= MAX_NUMBER-1){
-        menu[selectedItemIndex].setColor(sf::Color::White);
+        menu[selectedItemIndex].setFillColor(sf::Color::White);
         selectedItemIndex++;
-        menu[selectedItemIndex].setColor(sf::Color::Magenta);
+        menu[selectedItemIndex].setFillColor(sf::Color::Magenta);
     }
 }
 void Menu::draw(sf::RenderWindow &window){
